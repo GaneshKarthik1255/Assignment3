64f3e458e5c64a09efe19e1f79f3b9ed8b771c22
GaneshKarthik1255 <ganeshkarthikhg@gmail.com>
2023-02-16 09:59:22 +0000

    changes comitted

--- h.java
+++ h.java
@@ -2,6 +2,6 @@ public class h
 {
         public static void main(String[] args)
         {
-                System.out.println("hello");
+                System.out.println("hii welcome to devops");
         }
 }
